74de90755bd97126aa1561054f843d55448642ee
Jason Malfatto <jmalfatto@gmail.com>
2016-05-26 10:48:26 -0400

    adds multi-file save support and fixes bug on upload form 1

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var server = require('http').Server(app);
 var formidable = require('formidable');
 var fs = require('fs-extra');
 var io = require('socket.io')(server);
+var util = require('util');
 var serveIndex = require('serve-index');
 
 const uploadDir = __dirname + '/uploads/';
@@ -27,22 +28,27 @@ app.post('/upload', function (req, res) {
         console.log(uploadStatus);
         io.emit('news', { bytesReceived, bytesExpected });
     });
-    
-    form.parse(req, function (err, fields, files) {
-        for (var key in files) {
-            fs.readFile(files[key].path, function (err, data) {
-                var newPath = uploadDir + files[key].name;
-                fs.outputFile(newPath, data, (err) => {
-                    if (err) {
-                        console.log(err);
-                        return;
-                    }
-                    console.log('It\'s saved!');
-                });
-           });
+
+    form.parse(req, function(err, fields, files) {
+        res.writeHead(200, {'content-type': 'text/plain'});
+        res.write('Upload received :\n');
+        res.end(util.inspect({fields: fields, files: files}));
+    });
+
+    form.on('end', function (fields, files) {
+        for (var i=0; i< this.openedFiles.length; i++) {
+            var file = this.openedFiles[i];
+            var newPath = uploadDir + file.name;
+            fs.copy(file.path, newPath + file.name, function (err) {
+                if (err) {
+                    console.error(err);
+                } else {
+                    console.log('success!', file.name)
+                }
+            });
         }
     });
-    res.send('Thank you');
+
 });
 
 app.use('/', serveIndex(__dirname));
